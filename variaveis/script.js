4cd04f6cd2383486b13e12f9f9a2cde86adf829f
lorraine.silva <lorraine.silva@groupsoftware.com.br>
2023-01-03 20:00:20 -0300

    Aula 1
    Primeira aula revendo Js

---
+++ variaveis/script.js
@@ -0,0 +1,40 @@
+let nome = 'Lorraine';
+let idade = 21;
+let estaTrabalhando = true;
+console.log(nome, idade, estaTrabalhando);
+
+let preco = 22;
+let totalComprado = 6;
+let valorTotal = preco * totalComprado;
+console.log(valorTotal);
+
+
+let sobrenome = 'cristina',
+    cidadeMoradia = 'Florianopolis',
+    cargo = 'Desenvolvedora';
+console.log(sobrenome, cidadeMoradia, cargo);
+
+let semDefinir;
+console.log(semDefinir);
+
+/*Exercicio*/
+// Declarar uma variavel com seu nome
+// Declarar uma variavel com sua idade
+let Nome = 'Lorraine',
+    Idade = 21;
+console.log(Nome, Idade);
+
+// Declarar uma variavel com sua comida favorita e não atribuir valor
+//Atribuir valor a sua comida favorita
+let comidaFavorita;
+console.log(comidaFavorita);
+
+comidaFavorita = 'Pizza';
+console.log(comidaFavorita);
+//Declarar 5 variaveis diferentes sem valores
+let musicaFavorita;
+let lugarFavorito;
+let proximoObjetivo;
+let virarBilingue;
+let ler1livro = 'harry potter';
+console.log(ler1livro);
