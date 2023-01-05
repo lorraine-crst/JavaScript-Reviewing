afb8cd95d5bfa8cd5de9086c493835bcbd7c5915
lorraine.silva <lorraine.silva@groupsoftware.com.br>
2023-01-04 22:07:02 -0300

    Aula tres
    revendo sobre numeros e operadores

---
+++ numeros e operadores/script.js
@@ -0,0 +1,61 @@
+var soma = 10 + 50;
+    subtracao = 50 - 10;
+    multiplicacao = 10 * 50;
+    divisao = 50 / 10;
+    exponente = 5 ** 10;
+
+    console.log(soma, subtracao, multiplicacao, divisao, exponente);
+
+
+// NOT A NUMBER
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade;
+var pesoPorDois = peso / 2;
+console.log(peso, pesoPorDois);
+
+// Incremento e decremento
+var incremento = 5,
+    incrementando = incremento++;
+
+console.log(incremento);
+
+var decremento = 5,
+    descrementando = decremento--;
+console.log(decremento);
+
+var teste = 'Isto é um teste'
+    incremento = teste++;
+    decremento = teste--;
+console.log(decremento, incremento);
+
+
+/*Exercicio*/
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+// Crie duas expressões que retornem NaN
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade;
+var pesoPorDois = peso / 2;
+console.log(pesoPorDois);
+
+var divisao = 'Comprei 10' / 2;
+console.log(divisao);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma);
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var Incremento = 5,
+    Incrementando = Incremento++;
+console.log(Incremento);
+
+// Como dividir o peso por 2?
+var peso = 60;
+var pesoPorDois = peso / 2;
+console.log(pesoPorDois + ' Kg')
